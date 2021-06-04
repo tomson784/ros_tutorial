bb2dbb0b54d2d7334968a0e21dce8722847eb171
tomson784 <tomohito040759@gmail.com>
2021-06-05 02:07:01 +0900

    update

--- turtlebot3_sample/gazebo_rl/scripts/scan_map_plot.py
+++ turtlebot3_sample/gazebo_rl/scripts/scan_map_plot.py
@@ -4,14 +4,21 @@ import matplotlib.pyplot as plt
 
 ranges = np.loadtxt("../data/ranges.txt", delimiter=",")
 angle_min = np.loadtxt("../data/angle_min.txt")
+angle_max = np.loadtxt("../data/angle_max.txt")
+angle_increment = np.loadtxt("../data/angle_increment.txt")
+angle = angle_min
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+ax.scatter(0,0)
 for r in ranges:
-    #change infinite values to 0
+    # change infinite values to 0
     if math.isinf(r) == True:
-        r = 0
-    #convert angle and radius to cartesian coordinates
-    x = math.trunc((r * 30.0)*math.cos(angle_min + (-90.0*3.1416/180.0)))
-    y = math.trunc((r * 30.0)*math.sin(angle_min + (-90.0*3.1416/180.0)))
-
-    plt.scatter(x, y)
+        continue
+    # convert angle and radius to cartesian coordinates
+    x = r * math.cos(angle)
+    y = r * math.sin(angle)
+    print(x, y)
+    ax.scatter(x, y)
+    angle += angle_increment
 
 plt.show()
