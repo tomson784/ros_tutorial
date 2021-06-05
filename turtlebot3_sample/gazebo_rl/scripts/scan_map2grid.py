9dcb11be9aee1e1352db69ca8c325e59c3f45abf
tomson784 <tomohito040759@gmail.com>
2021-06-05 15:14:43 +0900

    update

--- turtlebot3_sample/gazebo_rl/scripts/scan_map2grid.py
+++ turtlebot3_sample/gazebo_rl/scripts/scan_map2grid.py
@@ -6,9 +6,9 @@ ranges = np.loadtxt("../data/ranges.txt", delimiter=",")
 angle_min = np.loadtxt("../data/angle_min.txt")
 angle_max = np.loadtxt("../data/angle_max.txt")
 angle_increment = np.loadtxt("../data/angle_increment.txt")
-angle = angle_min
+angle = angle_min + math.pi/2
 
-grid_num = 40
+grid_num = 80
 resolution = 0.1
 
 # 範囲の下限値が決まっている場合
@@ -18,16 +18,24 @@ def real2grid_index(x, y, x_min, y_min, resolution):
 
 # グリッド数が固定される場合
 def real2grid_index_fixed_grid_num(x, y, w, h, resolution):
-    return math.floor(x/resolution - w/2),\
-           math.floor(y/resolution - h/2)
+    return math.floor(x/resolution + w/2),\
+           math.floor(y/resolution + h/2)
+
+obs_map = np.zeros([grid_num, grid_num])
 
 for r in ranges:
-    # change infinite values to 0
     if math.isinf(r) == True:
+        angle += angle_increment
         continue
-    # convert angle and radius to cartesian coordinates
     x = r * math.cos(angle)
     y = r * math.sin(angle)
+    x_idx, y_idx = real2grid_index_fixed_grid_num(x, y, grid_num, grid_num, resolution)
+    # x_idx, y_idx = real2grid_index_fixed_grid_num(x, y, -3.5, -3.5, resolution)
+    obs_map[grid_num-y_idx][x_idx] = 1
     angle += angle_increment
 
+robot_x_idx, robot_y_idx = real2grid_index_fixed_grid_num(0, 0, grid_num, grid_num, resolution)
+obs_map[grid_num-robot_y_idx][robot_x_idx] = 2
 
+plt.imshow(obs_map)
+plt.show()
--- turtlebot3_sample/gazebo_rl/scripts/scan_map_plot.py
+++ turtlebot3_sample/gazebo_rl/scripts/scan_map_plot.py
@@ -6,19 +6,20 @@ ranges = np.loadtxt("../data/ranges.txt", delimiter=",")
 angle_min = np.loadtxt("../data/angle_min.txt")
 angle_max = np.loadtxt("../data/angle_max.txt")
 angle_increment = np.loadtxt("../data/angle_increment.txt")
-angle = angle_min
+angle = angle_min + math.pi/2
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
-ax.scatter(0,0)
+ax.scatter(0, 0, marker="x", color="red")
 for r in ranges:
     # change infinite values to 0
     if math.isinf(r) == True:
+        angle += angle_increment
         continue
     # convert angle and radius to cartesian coordinates
     x = r * math.cos(angle)
     y = r * math.sin(angle)
     print(x, y)
-    ax.scatter(x, y)
+    ax.scatter(x, y, marker="o", color="blue")
     angle += angle_increment
 
 plt.show()
